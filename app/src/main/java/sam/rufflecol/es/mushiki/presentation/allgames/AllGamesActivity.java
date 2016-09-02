508710728d7b9693a8f36b9f5df50d9a9350b2d5
Sam Ruffle Coles <samcoles@samcoles.co.uk>
2016-09-02 10:37:03 +0100

    make activity launchable

--- app/src/main/java/sam/rufflecol/es/mushiki/presentation/allgames/AllGamesActivity.java
+++ app/src/main/java/sam/rufflecol/es/mushiki/presentation/allgames/AllGamesActivity.java
@@ -3,15 +3,22 @@ package sam.rufflecol.es.mushiki.presentation.allgames;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import sam.rufflecol.es.mushiki.R;
 
 public class AllGamesActivity extends AppCompatActivity implements AllGamesMvp.View {
+
+    @BindView(R.id.rv_games)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_games);
+        ButterKnife.bind(this);
     }
 
 }
