b209956ce2142faef0fc0f91fab35a5423379061
Sam Ruffle Coles <samcoles@samcoles.co.uk>
2016-09-05 23:07:44 +0100

    tidy up allgamesactivity

--- app/src/main/java/es/rufflecol/sam/mushiki/presentation/allgames/AllGamesActivity.java
+++ app/src/main/java/es/rufflecol/sam/mushiki/presentation/allgames/AllGamesActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import javax.inject.Inject;
 
@@ -21,9 +20,6 @@ public class AllGamesActivity extends AppCompatActivity implements AllGamesMvp.V
 
     @BindView(R.id.rv_games)
     RecyclerView recyclerView;
-
-    @BindView(R.id.test)
-    TextView t;
 
     @Inject
     AllGamesMvp.Presenter presenter;
@@ -40,19 +36,20 @@ public class AllGamesActivity extends AppCompatActivity implements AllGamesMvp.V
 
     @Override
     public void requestSteamId() {
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this)
-                .setCancelable(false);
         LayoutInflater inflater = LayoutInflater.from(this);
         final EditText input = (EditText) inflater.inflate(R.layout.dialog_basic_editext, null);
-        dialogBuilder.setView(input);
-        dialogBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                String steamIdOrUsername = input.getText().toString();
-                presenter.fetchGamesForUser(steamIdOrUsername);
-            }
-        });
-        dialogBuilder.show();
+
+        new AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setView(input)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String steamIdOrUsername = input.getText().toString();
+                        presenter.fetchGamesForUser(steamIdOrUsername);
+                    }
+                })
+                .show();
     }
 
 }
